ce6f9a71c753cdec6b7c19352108f3724bf6687a
Alex <mboutchouangalex+github@gmail.com>
2023-03-25 03:01:47 +0100

    app layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import '@/styles/globals.css'
 import { Inter } from 'next/font/google';
 import { cn } from '@/lib/utils';
+import Providers from '@/components/Providers';
+import Navbar from '@/components/Navbar';
+import { Toaster } from "@/ui/Toast";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +18,15 @@ export default function RootLayout({
       className={cn('bg:white text-slate-900 antialiased', inter.className)}
     >
       <body className='min-h-screen bg-slate-50 dark:bg-slate-900 antialiased'>
-        {children}
+        <Providers>
+          {children}
+
+          <Toaster position='bottom-right' />
+          {/* @ts-expect-error Server Component */}
+          <Navbar />
+        </Providers>
+        {/* Allow for more hight for mobile devices */}
+        <div className='h-40 md:hidden' />
       </body>
     </html>
   )
